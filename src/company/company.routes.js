00c66b548f59b5c1415a32444a33d4d9fe54ba72
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-24 20:56:57 -0600

    se actualizan rutas

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { addCompany, updateCompany} from './company.controller.js'
 import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
-import { addCompanyValidator } from '../../middlewares/validators.js'
+import { addCompanyValidator, updateCompanyValidator } from '../../middlewares/validators.js'
 
 const api = Router()
 
 api.post('/add',[validateJwt,isAdmin,addCompanyValidator],addCompany)
-api.put('/update',[validateJwt,isAdmin,],updateCompany)
+api.put('/update',[validateJwt,isAdmin,updateCompanyValidator],updateCompany)
 export default api
