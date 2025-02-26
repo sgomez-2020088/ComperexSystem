4984aed2c68aae6765de1916277e9148433b051f
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-25 21:15:04 -0600

    se actualizan rutas

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { createCategory, getCategory, updateCategory} from '../category/category.controller.js'
-import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
+import { validateJwt, isAdmin } from '../../middlewares/validate.jwt.js'
+import { createCategoryValidator, updateCategoryValidator} from '../../middlewares/validators.js'
 
 
 const api = Router()
-api.post('/add',[validateJwt, isAdmin],createCategory)
+api.post('/add',[validateJwt,isAdmin, createCategoryValidator],createCategory)
 api.get('/get',[validateJwt, isAdmin],getCategory)
-api.put('/a',[validateJwt, isAdmin],updateCategory)
+api.put('/update',[validateJwt, isAdmin,updateCategoryValidator],updateCategory)
 
 
 export default api
