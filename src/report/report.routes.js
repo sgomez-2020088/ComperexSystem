56c5c77d7d93c3581b927b66d5badb633be86a9b
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-26 16:53:20 -0600

    se actualizan rutas

--- src/report/report.routes.js
+++ src/report/report.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { generateCompanyReport} from '../../src/report/report.controller.js'
+import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
 
 const api = Router()
 
-api.get('/reportExcel', generateCompanyReport)
+api.get('/reportExcel',[validateJwt, isAdmin], generateCompanyReport)
 
 export default api
