37c622c4743c78c099ba02e7d9251ed79261ab03
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-25 21:15:39 -0600

    se agrean funciones

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -27,3 +27,48 @@ export const updateCompany = async (req, res) => {
     }
     
 }
+
+export const getCompaniesCategory = async (req, res) => {
+    try {
+        const { categoryId } = req.body
+        const companies = await Company.find({ category: categoryId })
+        .populate('category','name -_id')
+        if (companies.length === 0) {
+            return res.status(404).send({ message: 'Companies not found', success: false })
+        }
+        return res.send({ message: 'Companies found', success: true, companies })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'General error',success: false })
+    }
+}
+
+export const getCompaniesTrajectory = async (req, res) => {
+    try {
+        const { trajectory } = req.body
+        const companies = await Company.find({ trajectory: { $gte: trajectory } })
+        if (companies.length === 0) {
+            return res.status(404).send({ message: 'Companies not found', success: false })
+        }
+        return res.send({ message: 'Companies found', success: true, companies })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'General error', success: false })
+    }
+}
+
+export const getCompaniesAlphabet = async (req, res) => {
+    try {
+        const { order } = req.body  
+        const sortOrder = order === 'desc' ? -1 : 1
+
+        const companies = await Company.find().sort({ name: sortOrder })
+        if (companies.length === 0) {
+            return res.status(404).send({ message: 'Companies not found', success: false })
+        }
+        return res.send({ message: 'Companies listed alphabetically', success: true, companies })
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'General error', success: false })
+    }
+}
